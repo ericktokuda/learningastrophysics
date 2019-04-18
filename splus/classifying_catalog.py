618ac82604756fc55088551ee6ed5f01d9b5f079
tokudaek <tokudaek@gmail.com>
2019-04-18 17:24:30 -0300

    Grid search

--- splus/classifying_catalog.py
+++ splus/classifying_catalog.py
@@ -3,13 +3,17 @@
 """
 
 import argparse
+import os
+import time
+import logging
+from logging import debug
+import pickle
+import itertools
+import numpy as np
+from copy import copy
 
 import matplotlib
 from matplotlib import pyplot as plt
-import os
-import logging
-import pickle
-
 
 import pandas as pd
 
@@ -38,7 +42,7 @@ def read_catalogs_dir(catalogdir, cols, _ids=None):
         catalogpath = os.path.join(catalogdir, f)
         if os.path.isdir(catalogpath): continue
 
-        logging.debug(catalogpath)
+        debug(catalogpath)
         df = pd.read_csv(catalogpath, sep='\s+', header=None, names=cols,
                          skiprows=191, engine='python')
         
@@ -73,28 +77,8 @@ def apply_regression(x_train, y_train, x_test, y_test):
     y_pred = regr.predict(x_test)
 
     #logging.debug('Coefficients: \n', regr.coef_)
-    logging.debug("Mean squared error: {:2f}\tVariance score: {:2f}"
+    debug("Mean squared error: {:2f}\tVariance score: {:2f}"
           .format(mean_squared_error(y_test, y_pred), r2_score(y_test, y_pred)))
-
-def apply_svm(x_train, y_train, x_test, y_test):
-    """Apply svm to the input data
-
-    Args:
-    x_train, x_test (2d tensor): (i, j) element represent j-th atribute of the i-th sample
-    y_train, y_test (list): its lenght is equal to the @x number of rows
-
-    Returns:
-    ret
-    """
-
-    clf = svm.SVC(C=1.0, cache_size=2000, class_weight=None, coef0=0.0,
-                  decision_function_shape='ovr', degree=3, gamma='scale', kernel='linear',
-                  max_iter=-1, probability=False, random_state=None, shrinking=True,
-                  tol=0.001, verbose=False)
-
-    clf.fit(x_train, y_train) 
-    y_pred = clf.predict(x_test)
-    logging.debug(clf.score(x_test, y_test))
 
 def get_kth_fold_traintest_indices(k, total, testratio):
     """Partition the indices (testratio, 1-trainratio)
@@ -112,16 +96,70 @@ def get_kth_fold_traintest_indices(k, total, testratio):
     train = allindices.difference(test)
     return train, test
 
+def apply_svm(x, y, cv):
+    """Apply svm to the input data
+
+    Args:
+    x (2d tensor): (i, j) element represent j-th atribute of the i-th sample
+    y (list): ground-truth
+
+    Returns:
+    ndarray: (i, j) element represent i-th score from the i-th run
+    list: list of tuples of the values of the parameters
+    """
+
+    c = [.1, 1, 10]
+    degree = [2, 3]
+    gamma = ['auto', 'scale']
+    kernel = ['linear', 'poly', 'rbf', 'sigmoid']
+    params = itertools.product(c, degree, gamma, kernel)
+    paramslist = list(copy(params))
+
+    k = cv.get_n_splits()
+    allscores = np.ndarray((0, k))
+
+    for p in params:
+        debug('##########################################################')
+        debug('Cross-validation for {}...'.format(id))
+        
+        t0 = time.time()
+        
+        clf = svm.SVC(C=p[0],
+                      cache_size=2000,
+                      class_weight=None,
+                      coef0=0.0,
+                      decision_function_shape='ovr',
+                      degree=p[1],
+                      gamma=p[2],
+                      kernel=p[3],
+                      max_iter=2000,
+                      probability=False,
+                      random_state=None,
+                      shrinking=True,
+                      tol=0.001,
+                      verbose=False)
+        debug(clf)
+        scores = cross_val_score(clf, x, y, cv=cv)
+       
+        debug(scores)
+        allscores = np.concatenate((allscores, scores.reshape(1, k)), axis=0)
+        debug('Elapsed time: {:.2f}s'.format(time.time() - t0))
+    return allscores, paramslist
+
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     #parser.add_argument('--outdir', required=True, help='Output directory')
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG)
 
-    classespath = '/home/frodo/temp/raw-data/sloan_splus_matches.csv'
+    #classespath = '/home/frodo/temp/raw-data/sloan_splus_matches.csv'
+    classespath = '/home/frodo/temp/raw-data/sloan_splus_matches_sample500.csv'
     catalogdir = '/home/frodo/temp/raw-data/catalogs/'
     headerpath = 'data/header.txt'
-    catalogspklpath = 'data/catalogs.pkl'
+    resultsdir = 'results'
+
+    classesfname = os.path.basename(classespath)
+    catalogspklpath = os.path.join('data/', os.path.splitext(classesfname)[0] + '.pkl')
     aperture = 'auto' # ['auto'  'petro'  'aper' ]
 
     cols, broad_suff, narrow_suff = read_headers_and_identifiers(headerpath)
@@ -140,7 +178,7 @@ def main():
 
     features_broad = features[['ID'] + broadbands]
     features_narrow = features[['ID'] + narrowbands]
-    features_all = features[['ID'] + broadbands + narrowbands]
+    features_union = features[['ID'] + broadbands + narrowbands]
 
     classes_map = {}    # Create a map of numbers->classes
     for acc, a in enumerate(set(classes['class'])): classes_map[a] = acc
@@ -153,16 +191,39 @@ def main():
 
     x_broad = features_broad.sort_values(by='ID')[broadbands]
     x_narrow = features_narrow.sort_values(by='ID')[narrowbands] # Narrowbands
-    x_all = features_all.sort_values(by='ID')[broadbands + narrowbands] # All bands
+    x_union = features_union.sort_values(by='ID')[broadbands + narrowbands] # All bands
 
     cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
-    for x in [x_broad, x_narrow, x_all]:
-        clf = svm.SVC(C=1.0, cache_size=2000, class_weight=None, coef0=0.0,
-                      decision_function_shape='ovr', degree=3, gamma='scale', kernel='linear',
-                      max_iter=-1, probability=False, random_state=None, shrinking=True,
-                      tol=0.001, verbose=False)
-        scores = cross_val_score(clf, x, y, cv=cv)
-        logging.debug(scores)
+    broadscores, params = apply_svm(x_broad, y, cv)
+    narrowscores, _ = apply_svm(x_narrow, y, cv)
+    unionscores, _ = apply_svm(x_union, y, cv)
+
+    params_path = os.path.join(resultsdir,
+                                      os.path.splitext(classesfname)[0] + '_params.pkl')
+    results_broad_path = os.path.join(resultsdir,
+                                      os.path.splitext(classesfname)[0] + '_broadscores.pkl')
+    results_narrow_path = os.path.join(resultsdir,
+                                      os.path.splitext(classesfname)[0] + '_narrowscores.pkl')
+    results_union_path = os.path.join(resultsdir,
+                                      os.path.splitext(classesfname)[0] + '_unionscores.pkl')
+
+    pickle.dump(params, open(params_path, 'wb'))
+    pickle.dump(broadscores, open(results_broad_path, 'wb'))
+    pickle.dump(narrowscores, open(results_narrow_path, 'wb'))
+    pickle.dump(unionscores, open(results_union_path, 'wb'))
+
+    # Generate output
+    n = len(params)
+    bm = broadscores.mean(axis=1).reshape(n, 1)
+    bs = broadscores.std(axis=1).reshape(n, 1)
+    nm = narrowscores.mean(axis=1).reshape(n, 1)
+    ns = narrowscores.std(axis=1).reshape(n, 1)
+    um = unionscores.mean(axis=1).reshape(n, 1)
+    us = unionscores.std(axis=1).reshape(n, 1)
+    summary = np.concatenate([np.array(params), bm, bs, nm, ns, um, us], axis=1)
+    h = 'C,deg,gamma,ker,broadmean,broadstd,narrowmean,narrowstd,unionmean,unionstd'
+    np.savetxt('/tmp/out.csv', summary, fmt='%s', delimiter=',',
+               header=h, comments='')
 
 
 ##########################################################
